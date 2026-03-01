37a9bb7d1b0dd6c36fcbd029062d97cf167d635b
Arnav-Sirhkal <Abhinavsirkhal@gmail.com>
2026-03-01 20:47:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const passwordBox = document.getElementById("password");
+const copyBtn = document.getElementById("copyBtn");
+const generateBtn = document.getElementById("generateBtn");
+
+const length = 15;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "1234567890";
+const symbol = "@!#$%^&*~";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+generateBtn.addEventListener("click", createPassword);
+
+copyBtn.addEventListener("click", () => {
+  if (password.value === "") {
+    return;
+  }
+  navigator.clipboard.writeText(password.value);
+  copyBtn.style.filter = "brightness(1.5)";
+
+  setTimeout(() => {
+    copyBtn.style.filter = "brightness(1)";
+  }, 200);
+});
